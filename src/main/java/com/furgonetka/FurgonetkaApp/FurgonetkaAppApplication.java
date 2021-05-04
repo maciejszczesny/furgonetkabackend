e8c3c1e5e3c90b896ffc23177ba1e38f807d8b92
Maciek-Szczesny <m.szcze15@gmail.com>
2021-05-04 17:11:12 +0200

    First push to backend

---
+++ src/main/java/com/furgonetka/FurgonetkaApp/FurgonetkaAppApplication.java
@@ -0,0 +1,13 @@
+package com.furgonetka.FurgonetkaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurgonetkaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurgonetkaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furgonetka/FurgonetkaApp/FurgonetkaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furgonetka.FurgonetkaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurgonetkaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
